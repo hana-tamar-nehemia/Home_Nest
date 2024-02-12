e49610369b552c5b3f95afcd193a16a39b616ba4
Tamar Nehemia <tamar5388@gmail.com>
2024-02-12 23:21:48 +0200

    working on Slide and Categories in the Home page

---
+++ src/redux/state.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state, action) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const {
+  setLogin,
+  setLogout,
+  setListings,
+  setTripList,
+  setWishList,
+  setPropertyList,
+  setReservationList,
+} = userSlice.actions;
+export default userSlice.reducer;
